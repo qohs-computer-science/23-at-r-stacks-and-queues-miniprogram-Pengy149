f1c5b61e9518c5d99df908c53eb9e23401486938
Pengy149 <SuperNoobz567@gmail.com>
2024-12-10 15:20:50 +0000

    Kevin Zheng - 12/10 - Finished/Ready for Grading

--- StackQueueMini.java
+++ StackQueueMini.java
@@ -1,21 +1,23 @@
 /*
  * Name: Kevin Zheng 
- * Date: 12/09/2024 
+ * Date: 12/10/2024 
  * Class Period: 3 
  * File Name: StackQueueMini.java 
- * Program Description: The purpose of this java file is to 
+ * Program Description: The purpose of this program is to first ask the user for 5 whole numbers for a stack and then duplicate each number in that stack but still maintain the 
+ * same order as the original order. Then, the program asks the user for 10 whole numbers for a queue and then changes the order so that the odd numbers are located 
+ * after the even numbers. 
  */
 
 // Scanner is imported in order to be able to get user input. 
 import java.util.Scanner;
 
-// 
+// Stack is imported in order to be able to temporarily store user inputs  
 import java.util.Stack;
 
-// 
+// Queue is imported to be able to temporarily store user inputs 
 import java.util.Queue;
 
-// 
+// LinkedList is imported to be used as initial value for Queue 
 import java.util.LinkedList;
 
 class StackQueueMini {
@@ -43,35 +45,38 @@ class StackQueueMini {
 		{
 			Integer temp = dupStk.pop();
 			System.out.println(temp);
-		}
+		} // end while loop 
 		System.out.println("Bottom of Stack");
 
 
-    Queue <Integer> firstStk = new LinkedList<Integer>(); 
-    Queue <Integer> oddQ = new LinkedList<Integer>(); 
+    Queue <Integer> initialQ = new LinkedList<Integer>(); 
+    Queue <Integer> newQ = new LinkedList<Integer>(); 
 		for(int i = 0; i < 10; i++) {
       System.out.println("Enter a whole number: ");
       userNum = userIn.nextInt();
-      firstQ.add(userNum);
+      initialQ.add(userNum);
     } // end for loop  		
 
-    if(userNum % 2 != 0)
-      oddQ.add(userNum);
-    else 
-      firstQ.add(userNum);
+    for(int i = 0; i < 10; i++) {
+      Integer temp = initialQ.remove();
+      if(temp % 2 == 0) 
+        newQ.add(temp);
+      else 
+        initialQ.add(temp);
+		} // end for loop 
 
-    while(!oddQ.isEmpty())
+    while(!initialQ.isEmpty())
 		{
-			Integer temp = oddQ.remove();
-			firstQ.add(temp);
-		}
+			Integer temp = initialQ.remove();
+			newQ.add(temp);
+		} // end while loop 
 
     System.out.println("Front of Queue");
-		while(!firstQ.isEmpty())
+		while(!newQ.isEmpty())
 		{
-			Integer temp = firstQ.remove();
+			Integer temp = newQ.remove();
 			System.out.println(temp);
-		}
+		} // end while loop 
 		System.out.println("Back of Queue");
 
   } // end main
